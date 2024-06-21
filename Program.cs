b6f5e43bbfa2403ff93cfc35548d8c08f6e0faad
Guillaume Dube <gdube@sajb.ca>
2024-06-21 09:49:15 -0400

    Init

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FindLocks {
+    class Program {
+        static void Main(string[] args) {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json", false, false)
+                .Build();
+            var appSettings = new AppSettings();
+            config.Bind(appSettings);
+            // --
+            if (args.Length == 0) {
+                Console.WriteLine("You must specify a (partial) file path to search.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(appSettings.HandlePath)) {
+                Console.WriteLine($"The Handle.exe path wasn't specified in appSettings.");
+                return;
+            }
+            if (!System.IO.File.Exists(appSettings.HandlePath)) {
+                Console.WriteLine($"The Handle.exe wasn't found: {appSettings.HandlePath}");
+                return;
+            }
+            var proc = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = appSettings.HandlePath,
+                    Arguments = $"-accepteula -u {args[0]}",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                }
+            };
+            proc.Start();
+            var handles = new List<Handle>();
+            var pattern = @"^(?<PNAME>[^ ]*)\s*pid: (?<PID>[0-9]*)\s*type: (?<TYPE>[^ ]*)\s*(?<USER>[^ ]*)\s*(?<HID>.*?): (?<HNAME>.*)";
+            var regex = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            while (!proc.StandardOutput.EndOfStream) {
+                var line = proc.StandardOutput.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                var matches = regex.Matches(line);
+                if (matches.Count == 0) continue;
+                var groups = matches[0].Groups;
+                var handle = new Handle();
+                handles.Add(handle);
+                handle.ProcessName = groups.GetValueOrDefault("PNAME").Value;
+                handle.ProcessId = groups.GetValueOrDefault("PID").Value;
+                handle.Type = groups.GetValueOrDefault("TYPE").Value;
+                handle.User = groups.GetValueOrDefault("USER").Value;
+                handle.HandleId = groups.GetValueOrDefault("HID").Value;
+                handle.Name = groups.GetValueOrDefault("HNAME").Value;
+            }
+            var results = handles.Select(x => new { x.ProcessName, x.ProcessId, x.User }).Distinct();
+            foreach (var x in results)
+                Console.WriteLine($"{x.User}: {x.ProcessName} ({x.ProcessId})");
+        }
+    }
+
+    class AppSettings { 
+        public string HandlePath { get; set; }
+    }
+
+    class Handle {
+        public string ProcessName { get; set; }
+        public string ProcessId { get; set; }
+        public string Type { get; set; }
+        public string User { get; set; }
+        public string HandleId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,14 @@
+﻿# README
+2021-06-10 DUBEG:
+Simple utility wrapping Sysinternal's Handle.exe.
+It merely simplifies its output to a list of tuple (User: Process).
+
+I use it within a powershell function (in my $PROFILE) 
+to find out who's locking either the Acomba company RENDAC or SAJB,
+whenever people from accounting want to obtain exclusive access 
+to either of them.
+
+It might either be an Acomba or a Servicentre process.
+Servicentre locks Aco files via the AcoSDK.
+
+It's regularly useful, around the day 10 of a month, to close the previous month.
